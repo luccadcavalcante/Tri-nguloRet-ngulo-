770fa030eee6a655567d46dc1f8e7e72d06abd46
luccadcavalcante <luccadcavalcante@gmail.com>
2024-09-02 11:39:23 -0300

    Update TrR.cs

---
+++ TrR.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    Console.readLine("")
+    for (int j = 0; j <= 5; j++){
+	    	for (int i = 1; i <= j; i++){
+		      Console.Write("*");
+    	 };
+         Console.WriteLine("");
+         
+   	  };
+    }
+  }
+}
